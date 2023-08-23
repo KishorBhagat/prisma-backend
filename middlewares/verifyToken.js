2d998aebd37d43e1055932ddd9f43ebd0ab3b124
Kishor Bhagat <kishorebhagat663@gmail.com>
2023-08-23 23:02:12 +0530

    added token verification middleware

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const prisma = require('../prisma/index');
+const jwt = require('jsonwebtoken');
+
+const verifyToken = async (req, res, next) => {
+    try {
+        const token = req.cookie.token;
+        if(!token) {
+            return res.json("Please login");
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await prisma.user.findUnique({
+            where: {
+                id: decoded.userId
+            }
+        });
+        next();
+    }
+    catch (error) {
+        throw new Error(error);
+    }
+}
